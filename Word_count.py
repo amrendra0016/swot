5a5cf513115de6d7a0cb9903a943bc2d994022ba
amrendra0016 <59682044+amrendra0016@users.noreply.github.com>
2020-04-13 11:26:46 +0530

    Word count Program updated1

--- Word_count.py
+++ Word_count.py
@@ -1,2 +1,14 @@
-Creating a word count pyspark project
-file created
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+def functionToCreateContext():
+ ssc = StreamingContext(sc, 10)
+ lines = ssc.textFileStream("file:///Users/amrendraupadhyay/Downloads/Amrendra/")
+ words = lines.flatMap(lambda line: line.split(" "))
+ pairs = words.map(lambda word: (word, 1))
+ windowedWordCounts = pairs.reduceByKeyAndWindow(lambda x, y: x + y, lambda x, y: x - y, 30, 10)
+ windowedWordCounts.saveAsTextFiles("file:///Users/amrendraupadhyay/Downloads/nath")
+ ssc.checkpoint("file:///Users/amrendraupadhyay/Downloads/Upadhyay_checkpoint") // setting up the checkpoint
+ return ssc
+context = StreamingContext.getOrCreate(“file:///Users/amrendraupadhyay/Downloads/Upadhyay_checkpoint”,functionToCreateContext)
+context.start()
+context.awaitTermination()
